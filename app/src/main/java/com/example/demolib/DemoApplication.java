f95f7a9140763c7a0a2da36c99df1586c657adc5
rel4 <7444670@qq.com>
2017-06-06 20:39:17 +0800

    fresco 显示图片

--- app/src/main/java/com/example/demolib/DemoApplication.java
+++ app/src/main/java/com/example/demolib/DemoApplication.java
@@ -4,7 +4,20 @@ import android.app.Application;
 
 import com.example.demolib.mvp.component.ApplicationComponent;
 import com.example.demolib.mvp.component.DaggerApplicationComponent;
+import com.example.demolib.mvp.module.ApiModule;
 import com.example.demolib.mvp.module.ApplicationModule;
+import com.facebook.cache.disk.DiskCacheConfig;
+import com.facebook.common.internal.Supplier;
+import com.facebook.common.util.ByteConstants;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.backends.okhttp3.OkHttpImagePipelineConfigFactory;
+import com.facebook.imagepipeline.cache.MemoryCacheParams;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.facebook.imagepipeline.decoder.SimpleProgressiveJpegConfig;
+
+import javax.inject.Inject;
+
+import okhttp3.OkHttpClient;
 
 
 /**
@@ -12,16 +25,45 @@ import com.example.demolib.mvp.module.ApplicationModule;
  */
 
 public class DemoApplication extends Application {
+    public static final int MAX_DISK_CACHE_SIZE = 50 * ByteConstants.MB;
+    private static final int MAX_HEAP_SIZE = (int) Runtime.getRuntime().maxMemory();
+    public static final int MAX_MEMORY_CACHE_SIZE = MAX_HEAP_SIZE / 8;
     private static ApplicationComponent sAppComponent;
+
+    @Inject
+    OkHttpClient mOkHttpClient;
 
     @Override
     public void onCreate() {
         super.onCreate();
         sAppComponent = DaggerApplicationComponent.builder()
+                .apiModule(new ApiModule())
                 .applicationModule(new ApplicationModule(this)).build();
+        sAppComponent.inject(this);
+        initFrescoConfig();
     }
 
     public static ApplicationComponent getApplicationComponent() {
         return sAppComponent;
+    }
+    private void initFrescoConfig() {
+        final MemoryCacheParams bitmapCacheParams =
+                new MemoryCacheParams(MAX_MEMORY_CACHE_SIZE, // Max total size of elements in the cache
+                        Integer.MAX_VALUE,                     // Max entries in the cache
+                        MAX_MEMORY_CACHE_SIZE, // Max total size of elements in eviction queue
+                        Integer.MAX_VALUE,                     // Max length of eviction queue
+                        Integer.MAX_VALUE);
+        ImagePipelineConfig config = OkHttpImagePipelineConfigFactory.newBuilder(this, mOkHttpClient)
+                .setProgressiveJpegConfig(new SimpleProgressiveJpegConfig())
+                .setBitmapMemoryCacheParamsSupplier(new Supplier<MemoryCacheParams>() {
+                    public MemoryCacheParams get() {
+                        return bitmapCacheParams;
+                    }
+                })
+                .setMainDiskCacheConfig(
+                        DiskCacheConfig.newBuilder(this).setMaxCacheSize(MAX_DISK_CACHE_SIZE).build())
+                .setDownsampleEnabled(true)
+                .build();
+        Fresco.initialize(this, config);
     }
 }
--- app/src/main/java/com/example/demolib/MainActivity.java
+++ app/src/main/java/com/example/demolib/MainActivity.java
@@ -3,16 +3,27 @@ package com.example.demolib;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.demolib.api.MainApi;
 import com.example.demolib.bean.BaseHttpFunc;
 import com.example.demolib.bean.DataBean;
 import com.example.demolib.components.exception.HttpServiceException;
+import com.example.demolib.mvp.component.DaggerApplicationComponent;
+import com.example.demolib.mvp.module.ApiModule;
 import com.example.demolib.utils.Logger;
 import com.example.demolib.utils.PhoneInfoUtils;
+import com.facebook.drawee.view.DraweeView;
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -23,8 +34,8 @@ import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.tv)
-    TextView tv;
+    @BindView(R.id.rv)
+    RecyclerView rv;
     @Inject
     Context mContext;
 
@@ -37,9 +48,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         DemoApplication.getApplicationComponent().inject(this);
-        String string = mContext.getResources().getString(R.string.app_name);
-        String deviceInfo = PhoneInfoUtils.getDeviceInfo(mContext);
-        tv.setText(deviceInfo);
+        initView();
         mainApi.getData().observeOn(AndroidSchedulers.mainThread())
                 .map(new BaseHttpFunc<List<DataBean>>())
                 .subscribe(new Subscriber<List<DataBean>>() {
@@ -50,17 +59,61 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onError(Throwable e) {
-                        if(e instanceof HttpServiceException)
-                        Logger.e("onError : " + ((HttpServiceException)e).toString());
+                        if (e instanceof HttpServiceException)
+                            Logger.e("onError : " + ((HttpServiceException) e).toString());
                     }
 
                     @Override
                     public void onNext(List<DataBean> s) {
+                        MyAdapter myAdapter = new MyAdapter();
+                        rv.setAdapter(myAdapter);
+                        myAdapter.addData(s);
                         Gson gson = new Gson();
                         Logger.e(gson.toJson(s));
                     }
                 });
 
+
+    }
+
+    private void initView() {
+//        LinearLayoutManager  mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rv.setLayoutManager(new GridLayoutManager(mContext, 2));
+    }
+
+    public class MyAdapter<T> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        private List datas = new ArrayList<T>();
+
+        public void addData(List<T> data) {
+            datas.addAll(data);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = View.inflate(parent.getContext(), R.layout.vh, null);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            DataBean bean = (DataBean) datas.get(position);
+            ((MyViewHolder) holder).iv.setImageURI(bean.getWallPaperUrls());
+        }
+
+        @Override
+        public int getItemCount() {
+            return datas.size();
+        }
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public SimpleDraweeView iv;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            iv = (SimpleDraweeView) itemView.findViewById(R.id.iv);
+        }
 
     }
 }
