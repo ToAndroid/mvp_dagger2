820038e4d30c81a988f5ae14b2cc36bf93ff79ca
rel4 <7444670@qq.com>
2017-07-24 23:56:24 +0800

    网络上传类

--- app/src/main/java/com/example/demolib/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/demolib/ui/login/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.demolib.ui.login;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -15,6 +16,7 @@ import android.widget.EditText;
 
 import com.example.demolib.App;
 import com.example.demolib.R;
+import com.example.demolib.updata.UploadNet;
 import com.example.demolib.utils.Logger;
 
 import javax.inject.Inject;
@@ -63,6 +65,7 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
                 .inject(this);
         mLoginPresenter.attachView(this);
     }
+
     public void initToolBar(Toolbar mToolBar) {
         if (null != mToolBar) {
             setSupportActionBar(mToolBar);
@@ -70,6 +73,7 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
+
     class MTextWatcher implements TextWatcher {
 
         TextInputLayout textInputLayout;
@@ -110,10 +114,41 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
     }
 
     private void doLogin() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+//                String s = UploadNet.get("http://blog.csdn.net/u012527802/article/details/51153071");
+                String downUlr = "http://app.cet4free.cn/voicedata/puregirls.zip";
+                UploadNet.downFile(downUlr, getApplication().getFilesDir().getPath(), "puregirls.zip", new UploadNet.DownFileListenter() {
+                    @Override
+                    public void onStart() {
+                        Logger.e("onStart");
+                    }
+
+                    @Override
+                    public void onProgress(long total, long progress) {
+                        Logger.e("onProgress : " + progress + " / " + total);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        Logger.e("onFinish");
+                    }
+
+                    @Override
+                    public void onDownError() {
+                        Logger.e("onDownError");
+                    }
+                });
+            }
+        });
+        thread.start();
+
+
         String username = etUserName.getText().toString().trim();
         String pwd = etPassWord.getText().toString().trim();
-        if (TextUtils.isEmpty(username)||TextUtils.isEmpty(pwd))return;
-        mLoginPresenter.login(username,pwd);
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(pwd)) return;
+//        mLoginPresenter.login(username,pwd);
     }
 
 }
---
+++ app/src/main/java/com/example/demolib/updata/UploadNet.java
@@ -0,0 +1,143 @@
+package com.example.demolib.updata;
+
+import android.text.TextUtils;
+
+import com.example.demolib.utils.Logger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ * Created by jb on 2017/7/24.
+ */
+
+public class UploadNet {
+    private final static int READ_TIMEOUT = 3000;
+    private final static int CONNECT_TIMEOUT = 3000;
+
+    public static String get(String urlStr) {
+        if (TextUtils.isEmpty(urlStr)) return null;
+
+        try {
+            URL url = new URL(urlStr);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(READ_TIMEOUT);
+            urlConnection.setConnectTimeout(CONNECT_TIMEOUT);
+            urlConnection.setRequestMethod("GET");
+            int responseCode = urlConnection.getResponseCode();
+
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+            if (responseCode == 200 || responseCode == 206) {
+                byte[] buffer = new byte[1024];
+                InputStream inputStream = urlConnection.getInputStream();
+
+                int len = 0;
+                while ((len = inputStream.read(buffer)) != -1) {
+                    bos.write(buffer, 0, len);
+                }
+                bos.close();
+
+            }
+            String s = new String(bos.toByteArray());
+            Logger.e(s);
+
+            return s;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void downFile(String downUrl, String savePath, String filweName, DownFileListenter listenter) {
+
+        if (TextUtils.isEmpty(downUrl) || TextUtils.isEmpty(savePath) || TextUtils.isEmpty(filweName))
+            return;
+
+        try {
+            URL url = new URL(downUrl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(READ_TIMEOUT);
+            urlConnection.setConnectTimeout(CONNECT_TIMEOUT);
+            urlConnection.setRequestMethod("GET");
+            int responseCode = urlConnection.getResponseCode();
+
+
+            if (responseCode == 200 || responseCode == 206) {
+
+                File dirFile = new File(savePath);
+                if (!dirFile.exists()) {
+                    dirFile.mkdirs();
+                }
+
+                File saveFile = new File(savePath, filweName);
+                if (!saveFile.exists()) {
+                    saveFile.createNewFile();
+                }
+
+                byte[] buffer = new byte[1024];
+                InputStream inputStream = urlConnection.getInputStream();
+                int contentLength = urlConnection.getContentLength();
+
+
+                FileOutputStream fileOutputStream = new FileOutputStream(saveFile);
+                long size = 0;
+                int len = 0;
+                if (listenter != null) {
+                    listenter.onStart();
+                }
+                while ((len = inputStream.read(buffer)) != -1) {
+                    fileOutputStream.write(buffer, 0, len);
+                    size += len;
+                    if (listenter != null) {
+                        listenter.onProgress(contentLength, size);
+                    }
+                }
+                fileOutputStream.close();
+                if (listenter != null) {
+                    listenter.onFinish();
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            if (listenter != null) {
+                listenter.onDownError();
+            }
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+            if (listenter != null) {
+                listenter.onDownError();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            if (listenter != null) {
+                listenter.onDownError();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (listenter != null) {
+                listenter.onDownError();
+            }
+        }
+    }
+
+    public interface DownFileListenter {
+        void onStart();
+
+        void onProgress(long total, long progress);
+
+        void onFinish();
+
+        void onDownError();
+    }
+}
